85146a66062ed42146013f4149828aea12164cbc
Tausif <tausif00100@gmail.com>
2026-02-21 14:59:03 +0530

    added auth

--- app/(auth)/sign-up/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-
-// Sign-up is now handled by the magic link flow on the login page.
-// Any visits to this route are redirected there.
-export default function Page() {
-  redirect('/auth/login')
-}
